aab386e104e661c4a66d729c22a40902f7660179
vikramr <vikramr@myPc>
2019-07-21 18:54:28 +0530

    initial commit

---
+++ src/main/java/com/performance/itemReactiveMockedDb/MockDbService.java
@@ -0,0 +1,13 @@
+package com.performance.itemReactiveMockedDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockDbService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockDbService.class, args);
+	}
+
+}
