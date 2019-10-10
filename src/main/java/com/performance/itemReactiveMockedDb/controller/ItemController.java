41589c37aa8ba1a6f0aa5b2b1dcbd77b97f27707
vikramrawat <vikramrawat@localhost>
2019-10-10 23:54:02 +0530

    final refactoring

--- src/main/java/com/performance/itemReactiveMockedDb/controller/ItemController.java
+++ src/main/java/com/performance/itemReactiveMockedDb/controller/ItemController.java
@@ -1,6 +1,10 @@
 package com.performance.itemReactiveMockedDb.controller;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,38 +22,76 @@ public class ItemController {
 //	@Autowired
 //	private ItemService itemService;
 
+	List<Item> itemList = new ArrayList<Item>();
+
+	@PostConstruct
+	private void postConstruct() {
+
+		for (int i = 0; i < 5000; i++) {
+
+			Item item = new Item();
+			item.setBrand("IBM");
+			item.setCategory("Laptop");
+			item.setDescription("HP Spectre");
+			this.itemList.add(item);
+
+		}
+	}
+
+	@GetMapping(path = "/items/stream/{category}", produces="application/stream+json")
+	public Flux<Item> getItemsbyCategoryStream(@PathVariable String category) {
+		Flux<Item> itemFlux = null;
+		if (category.equalsIgnoreCase("Laptop")) {
+			Item item1 = new Item();
+			item1.setBrand("IBM");
+			item1.setCategory("Laptop");
+			item1.setDescription("HP Spectre");
+
+			Item item2 = new Item();
+			item2.setBrand("Dell");
+			item2.setCategory("Laptop");
+			item2.setDescription("XPS");
+
+			Item item3 = new Item();
+			item3.setBrand("IBM");
+			item3.setCategory("Laptop");
+			item3.setDescription("HP Spectre");
+
+			itemFlux = Flux.just(item1, item2, item3);
+
+		} else {
+			itemFlux = Flux.fromIterable(this.itemList);
+
+		}
+		return itemFlux.delaySequence(Duration.ofMillis(200));
+	}
+	
 	@GetMapping(path = "/items/{category}")
 	public Flux<Item> getItemsbyCategory(@PathVariable String category) {
+		Flux<Item> itemFlux = null;
+		if (category.equalsIgnoreCase("Laptop")) {
+			Item item1 = new Item();
+			item1.setBrand("IBM");
+			item1.setCategory("Laptop");
+			item1.setDescription("HP Spectre");
 
-//		Runnable t = () -> {
-//			try {
-//				Thread.sleep(200);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		};
-//
-//		t.run();
+			Item item2 = new Item();
+			item2.setBrand("Dell");
+			item2.setCategory("Laptop");
+			item2.setDescription("XPS");
 
-		Item item1 = new Item();
-		item1.setBrand("IBM");
-		item1.setCategory("Laptop");
-		item1.setDescription("HP Spectre");
+			Item item3 = new Item();
+			item3.setBrand("IBM");
+			item3.setCategory("Laptop");
+			item3.setDescription("HP Spectre");
 
-		Item item2 = new Item();
-		item2.setBrand("Dell");
-		item2.setCategory("Laptop");
-		item2.setDescription("XPS");
+			itemFlux = Flux.just(item1, item2, item3);
 
-		Item item3 = new Item();
-		item3.setBrand("IBM");
-		item3.setCategory("Laptop");
-		item3.setDescription("HP Spectre");
+		} else {
+			itemFlux = Flux.fromIterable(this.itemList);
 
-		Flux<Item> itemFlux = Flux.just(item1, item2, item3);
+		}
 		return itemFlux.delaySequence(Duration.ofMillis(200));
-
 	}
 
 //	@PostMapping(path = "/items")
